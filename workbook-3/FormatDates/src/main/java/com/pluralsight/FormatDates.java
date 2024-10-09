63086fde56949c2130ec5e46bdfcbdc60aadc223
Furkan <furkanamerikaa@gmail.com>
2024-10-09 09:32:31 -0400

    2nd-attempt-FormattingDates

--- workbook-3/FormatDates/src/main/java/com/pluralsight/FormatDates.java
+++ workbook-3/FormatDates/src/main/java/com/pluralsight/FormatDates.java
@@ -3,13 +3,13 @@ import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 import java.util.Locale;
+import java.util.Scanner;
 
 
-
-    public class FormatDates {
+public class FormatDates {
         public static void main(String[] args) {
             // Get the current date and time
-            LocalDateTime currentDateTime = LocalDateTime.now();
+            /*LocalDateTime currentDateTime = LocalDateTime.now();
 
             // Define the different formats
             DateTimeFormatter format1 = DateTimeFormatter.ofPattern("09/05/2021");
@@ -18,13 +18,19 @@ import java.util.Locale;
             DateTimeFormatter format4 = DateTimeFormatter.ofPattern("Sunday, Sep 5, 2021 10:02").withZone(ZoneId.of("GMT"));
 
             // Display the current date and time in different formats
-            System.out.println(get.DateTime.format(format1));
+            System.out.println(date.get.DateTime.format(format1));
             System.out.println(get.Time.format(format2));
             System.out.println(get.Time.format(format3));
             System.out.println(format4.format(currentDateTime.atZone(ZoneId.systemDefault())));
+
+            // I need to change the dates with user`s input and I can do it with using getter.
+// or I can use scanner to get input from the user than display it.
+// I am trying but unfortunately can`t do it for now.
+// So, later on I will try to do it.
+        }*/
+
+            Scanner scanner = new Scanner(System.in);
         }
     }
 
-// I need to change the dates with user`s input and I can do it with using getter.
-// I am trying but unfortunately can`t do it for now.
-// So, later on I will try to do it.
+
