19286f7a1ff0c9e80ad55645f43448b725f2ee21
Furkan <furkanamerikaa@gmail.com>
2024-10-21 13:49:49 -0400

    workbook-4

--- workbook-4/HotelOperations/src/main/java/com/pluralsight/Employee.java
+++ workbook-4/HotelOperations/src/main/java/com/pluralsight/Employee.java
@@ -1,4 +1,10 @@
 package com.pluralsight;
 
 public class Employee {
+    private int employeeId;
+    private String name;
+    private String department;
+    private double payRate;
+    private double hourWorked;
+    
 }
