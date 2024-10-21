7f969cf559a2da604575e9d603bffc2afb51339f
Furkan <furkanamerikaa@gmail.com>
2024-10-21 14:18:50 -0400

    workbook-4

--- workbook-4/HotelOperations/src/main/java/com/pluralsight/Employee.java
+++ workbook-4/HotelOperations/src/main/java/com/pluralsight/Employee.java
@@ -5,6 +5,46 @@ public class Employee {
     private String name;
     private String department;
     private double payRate;
-    private double hourWorked;
+    private double hoursWorked;
 
+    public Employee(int employeeId, String name, String department, double payRate, double hoursWorked) {
+        this.employeeId = employeeId;
+        this.name = name;
+        this.department = department;
+        this.payRate = payRate;
+        this.hoursWorked = hoursWorked;
+    }
+
+    public double getRegularHours() {
+        return Math.min(hoursWorked, 40);
+    }
+
+    public double getOverTimeHours() {
+        return Math.max(0, hoursWorked-40);
+    }
+    public double getTotalPay() {
+        double regularHours = getRegularHours();
+        double overtimeHours = getOverTimeHours();
+        return (regularHours * payRate) + (overtimeHours * payRate *1.5);
+    }
+
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public double getPayRate() {
+        return payRate;
+    }
+
+    public double getHoursWorked() {
+        return hoursWorked;
+    }
 }
--- workbook-4/HotelOperations/src/main/java/com/pluralsight/Reservations.java
+++ workbook-4/HotelOperations/src/main/java/com/pluralsight/Reservations.java
@@ -44,5 +44,5 @@ public class Reservations {
     public void setWeekend(boolean weekend) {
         isWeekend = weekend;
     }
-    
+
 }
