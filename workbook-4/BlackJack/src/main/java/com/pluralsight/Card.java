572c1360b098556e1a95421b1bcb62659742a507
Furkan <furkanamerikaa@gmail.com>
2024-11-04 14:05:01 -0500

    Card method added and constructor generated

--- workbook-4/BlackJack/src/main/java/com/pluralsight/Card.java
+++ workbook-4/BlackJack/src/main/java/com/pluralsight/Card.java
@@ -1,4 +1,15 @@
 package com.pluralsight;
 
 public class Card {
+    private String suit;
+    private String value;
+    private boolean isFaceUp;
+
+    public Card(String suit, String value, boolean isFaceUp) {
+        this.suit = suit;
+        this.value = value;
+        this.isFaceUp = isFaceUp;
+    }
+
+    
 }
