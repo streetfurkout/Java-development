17e372e64dd34f1e48ecb64e15384d7868554794
Furkan <furkanamerikaa@gmail.com>
2024-11-04 13:54:53 -0500

    Shuffle method added

--- workbook-4/BlackJack/src/main/java/com/pluralsight/Deck.java
+++ workbook-4/BlackJack/src/main/java/com/pluralsight/Deck.java
@@ -1,17 +1,28 @@
 package com.pluralsight;
 
 import java.util.ArrayList;
+import java.util.Collection;
 
 public class Deck {
-    private ArrayList<Card>cards;
+    private ArrayList<Card> cards;
 
     public Deck() {
         cards = new ArrayList<>();
-        String[] suits = {"Hearths" , "Spades" , "Diamonds" , "Clubs"};
-        String[] values= {"2","3","4","5","6","7","8","9","J","Q","K","A"};
+        String[] suits = {"Hearths", "Spades", "Diamonds", "Clubs"};
+        String[] values = {"2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K", "A"};
 
-
-            
+        for (String suit : suits) {
         }
+        for (String value : values) {
+        }
+        Card card = new Card(suit, value);
+        cards.add(card);
     }
+    public shuffle() {
+        Collection.shuffle(cards);
+    }
+
+    }
+}
+
 
