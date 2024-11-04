6bb899728b14a14b5779a87f5ce3c87ddc42b1b1
Furkan <furkanamerikaa@gmail.com>
2024-11-04 14:18:59 -0500

    card class getValue created and updated

--- workbook-4/BlackJack/src/main/java/com/pluralsight/Card.java
+++ workbook-4/BlackJack/src/main/java/com/pluralsight/Card.java
@@ -18,6 +18,15 @@ public class Card {
             return "#";
         }
     }
-    
+    public String getValue() {
+        // only return the value if the card is face up
+        if (isFaceUp) {
+            // this is the string value of the card
+            // i.e. A, K, Q, J, 10, 9 ...
+            return value;
+        } else {
+            return "#";
+        }
+    }
 }
 
