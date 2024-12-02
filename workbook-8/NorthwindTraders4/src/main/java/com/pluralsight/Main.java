fda52cce144d7466e1b30b7df05f933c81bece9e
Furkan <furkanamerikaa@gmail.com>
2024-12-02 09:48:12 -0500

    odd number exercise from codewars solution tries

--- workbook-8/NorthwindTraders4/src/main/java/com/pluralsight/Main.java
+++ workbook-8/NorthwindTraders4/src/main/java/com/pluralsight/Main.java
@@ -1,19 +1,19 @@
 package com.pluralsight;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
+import java.sql.*;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
-        if(args.length != 2) {
-            System.out.println("We need 2 arguments!!");
+        // Check for command line arguments for username and password
+        if (args.length != 2) {
+            System.out.println("Application needs two arguments to run: " +
+                    "java com.pluralsight.Main <username> <password>");
             System.exit(1);
         }
+
         String username = args[0];
-        String password = args [1];
+        String password = args[1];
 
         Scanner scanner = new Scanner(System.in);
 
@@ -25,38 +25,72 @@ public class Main {
             System.out.println("0) Exit");
             System.out.print("Select an option: ");
 
-            int usersChoice = scanner.nextInt();
+            int choice = scanner.nextInt();
 
-            switch (usersChoice) {
-                case 1 :
+            switch (choice) {
+                case 1:
                     displayAllProducts(username, password);
                     break;
-                case 2 :
+                case 2:
                     displayAllCustomers(username, password);
                     break;
                 case 3:
                     displayAllCategories(username, password);
                     break;
-                case 0 :
-                    System.out.println("BYE!!");
+                case 0:
+                    System.out.println("Exiting...");
                     return;
-                default :
-                    System.out.println("Please try again (invalid option)");
+                default:
+                    System.out.println("Invalid option. Please try again.");
                     break;
             }
         }
     }
 
-    public static void displayAllProducts(String username, String password) {
-        try{
+    private static void displayAllCategories(String username, String password) {
+        try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            try(Connection connection = DriverManager.getConnection("jdbc:msql://localhost:3306/northwind",
+            try (Connection connection = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/northwind",
                     username,
                     password);
-                PreparedStatement statement = connection.prepareStatement(
-                        "SELECT CategoryID, CategoryName FROM"
-                )
+                 PreparedStatement statement = connection.prepareStatement(
+                         "SELECT CategoryID, CategoryName FROM Categories ORDER BY CategoryID");
+                 ResultSet resultSet = statement.executeQuery()) {
 
+                System.out.println("Categories:");
+                while (resultSet.next()) {
+                    int categoryId = resultSet.getInt("CategoryID");
+                    String categoryName = resultSet.getString("CategoryName");
+                    System.out.println(categoryId + ": " + categoryName);
+                }
+
+
+
+    private static void displayAllProducts(String username, String password) {
+        String query = "SELECT ProductID, ProductName, UnitPrice, UnitsInStock FROM products";
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            // Use try-with-resources for automatic resource management
+            try (Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/northwind", username, password);
+                 PreparedStatement statement = connection.prepareStatement(query);
+                 ResultSet results = statement.executeQuery()) {
+
+                while (results.next()) {
+                    int productId = results.getInt("ProductID");
+                    String productName = results.getString("ProductName");
+                    double unitPrice = results.getDouble("UnitPrice");
+                    int unitsInStock = results.getInt("UnitsInStock");
+
+                    System.out.println("Product ID: " + productId);
+                    System.out.println("Product Name: " + productName);
+                    System.out.println("Unit Price: " + unitPrice);
+                    System.out.println("Units In Stock: " + unitsInStock);
+                    System.out.println("-----------------------------------------");
+                }
+            }
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
         }
-    }
-}
