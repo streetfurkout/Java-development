d9ddcd88636f1d47efaeb20ddab1c797a462a110
Furkan <furkanamerikaa@gmail.com>
2024-12-03 13:58:00 -0500

    NorthwindShipper jdbc crud example shipper class created and attirubutes added, getters/setters generated and tostring overrided

---
+++ workbook-8/NorthwindShippers/src/main/java/com/pluralsight/db/Shipper.java
@@ -0,0 +1,42 @@
+package com.pluralsight.db;
+
+public class Shipper {
+    private int id;
+    private String name;
+    private int phone;
+
+    public Shipper(int id, String name, int phone) {
+        this.id = id;
+        this.name = name;
+        this.phone = phone;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPhone() {
+        return phone;
+    }
+
+    public void setPhone(int phone) {
+        this.phone = phone;
+    }
+
+    @Override
+    public String toString() {
+        return "Shipper{id=" + id + ", name='" + name + "', phone='" + phone + "'}";
+    }
+}
