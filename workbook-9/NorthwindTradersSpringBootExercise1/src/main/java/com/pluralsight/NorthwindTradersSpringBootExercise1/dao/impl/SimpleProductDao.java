10cc406bf2a800ff17ae160b41215684f00d763d
Furkan <furkanamerikaa@gmail.com>
2024-12-08 14:22:50 -0500

    IporductDao class completed inside of the interfaces package

--- workbook-9/NorthwindTradersSpringBootExercise1/src/main/java/com/pluralsight/NorthwindTradersSpringBootExercise1/dao/impl/SimpleProductDao.java
+++ workbook-9/NorthwindTradersSpringBootExercise1/src/main/java/com/pluralsight/NorthwindTradersSpringBootExercise1/dao/impl/SimpleProductDao.java
@@ -1,9 +1,21 @@
 package com.pluralsight.NorthwindTradersSpringBootExercise1.dao.impl;
 
 import com.pluralsight.NorthwindTradersSpringBootExercise1.dao.interfaces.IProductDao;
+import com.pluralsight.NorthwindTradersSpringBootExercise1.models.Product;
 import com.pluralsight.NorthwindTradersSpringBootExercise1.services.ProductService;
 
-public class SimpleProductDao implements IProductDao {
-    private 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
+public class SimpleProductDao implements IProductDao {
+    private List<Product> products;
+
+    public SimpleProductDao() {
+        this.products = new ArrayList<>();
+
+        products.add(new Product(1, "Cheddar", 1, 1.50));
+        products.add(new Product(2, "Steak", 4, 10));
+        products.add(new Product(3, "Steak", 5, 10));
+    }
 }
--- workbook-9/NorthwindTradersSpringBootExercise1/src/main/java/com/pluralsight/NorthwindTradersSpringBootExercise1/dao/interfaces/IProductDao.java
+++ workbook-9/NorthwindTradersSpringBootExercise1/src/main/java/com/pluralsight/NorthwindTradersSpringBootExercise1/dao/interfaces/IProductDao.java
@@ -1,4 +1,14 @@
 package com.pluralsight.NorthwindTradersSpringBootExercise1.dao.interfaces;
 
+import com.pluralsight.NorthwindTradersSpringBootExercise1.models.Product;
+
+import java.util.List;
+
 public interface IProductDao {
+    Product insert(Product product);
+
+    List<Product> getAll();
+    Product getById(int productId);
+    void update(int productId, Product product);
+    void delete(int productId);
 }
