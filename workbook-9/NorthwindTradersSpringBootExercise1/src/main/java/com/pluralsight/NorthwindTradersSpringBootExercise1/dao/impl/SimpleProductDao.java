55963853fb9d51bf49d900ea3081b29538122753
Furkan <furkanamerikaa@gmail.com>
2024-12-11 09:58:17 -0500

    try

--- workbook-9/NorthwindTradersSpringBootExercise1/src/main/java/com/pluralsight/NorthwindTradersSpringBootExercise1/dao/impl/SimpleProductDao.java
+++ workbook-9/NorthwindTradersSpringBootExercise1/src/main/java/com/pluralsight/NorthwindTradersSpringBootExercise1/dao/impl/SimpleProductDao.java
@@ -18,4 +18,45 @@ public class SimpleProductDao implements IProductDao {
         products.add(new Product(2, "Steak", 4, 10));
         products.add(new Product(3, "Steak", 5, 10));
     }
+
+    @Override
+    public Product insert(Product product) {
+        int maxId = 0;
+        for (Product t : products) {
+            if(t.getProductId()>maxId){
+                maxId =t.getProductId();
+            }
+        }
+        product.setProductId(maxId+1);
+        products.add(product);
+        return product;
+    }
+
+    @Override
+    public List<Product> getAll() {
+        return products;
+    }
+
+    @Override
+    public Product getById(int productId) {
+        for (Product product : products) {
+            if(product.getProductId()==productId){
+                return product;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public void update(int productId, Product product) {
+        int index = getProductIndex(productId);
+        if (index != -1) {
+            products.set(index, product);
+        }
+    }
+
+    @Override
+    public void delete(int productId) {
+
+    }
 }
